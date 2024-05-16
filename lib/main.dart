ff60a737b932afd7283e24ae835a0b583359da23
kingdrofd <kingdrofd@gmail.com>
2024-05-16 03:22:43 +0100

    update: main and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
+import 'package:general_mod_manager/components/modList/mod_list.dart';
+import 'package:general_mod_manager/components/options/options.dart';
+import 'package:general_mod_manager/services/directory_handler.dart';
+import 'package:general_mod_manager/services/modsProcess/mod_info_process.dart';
+import 'package:general_mod_manager/services/modsProcess/mod_model.dart';
+
+import 'package:general_mod_manager/utils/variables.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent),
       home: const MyHomePage(),
     );
   }
@@ -32,95 +37,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController _textEditingController = TextEditingController();
-  TextEditingController _textEditingControllerName = TextEditingController();
-  String messageData = "";
-  Future fetchEntry() async {
-    final url = Uri.parse('API HERE');
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      var content = jsonDecode(response.body);
-      setState(() {
-        messageData = content['message'];
-      });
+  Directories directories = Directories();
+  ModInfo modInfo = ModInfo();
+  List modsData = [];
+  List<Mod> extractedModsData = [];
+  // Future fetchEntry() async {
+  //   final url = Uri.parse('API HERE');
+  //   final response = await http.get(url);
+  //   if (response.statusCode == 200) {
+  //     var content = jsonDecode(response.body);
+  //     setState(() {
+  //       messageData = content['message'];
+  //     });
 
-      print(messageData);
-      return messageData;
-    } else {
-      print('Failed to fetch data');
-    }
-  }
+  //     print(messageData);
+  //     return messageData;
+  //   } else {
+  //     print('Failed to fetch data');
+  //   }
+  // }
 
-  Future<void> _sendData() async {
-    try {
-      if (_textEditingController.text.isNotEmpty) {
-        var url = Uri.parse("API HERE");
-        var headers = {"Content-Type": "application/json"};
-        var body = jsonEncode({
-          "name": _textEditingControllerName.text,
-          "path": _textEditingController.text
-        });
-        var response = await http.post(url, headers: headers, body: body);
-        if (response.statusCode == 200) {
-          // Data sent successfully
-          print("Data sent successfully");
-        } else {
-          // Failed to send data
-          print("Failed to send data");
-        }
-      }
-    } catch (e) {
-      print(e);
-    }
+  void _init() async {
+    modsData = await modInfo.loadModList();
+    extractedModsData = await modInfo.loadExtractedModList();
+    print(directories.getScriptFilesPath().path);
   }
 
   @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+
+  bool isHovered = false;
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 500,
-        width: 600,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Center(
-              child: ElevatedButton.icon(
-                  onPressed: () {
-                    fetchEntry();
-                  },
-                  icon: Icon(Icons.arrow_downward_rounded),
-                  label: Text("Get Data")),
-            ),
-            Text(messageData),
-            SizedBox(
-              width: 400,
-              height: 100,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 50,
-                    child: TextField(
-                      controller: _textEditingControllerName,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 100,
-                    height: 50,
-                    child: TextField(
-                      controller: _textEditingController,
-                    ),
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        _sendData();
-                      },
-                      icon: Icon(Icons.arrow_upward_rounded))
-                ],
-              ),
-            )
-          ],
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: backgroundColor,
+        title: Center(
+          child: Text(
+            "Baldur's Gate 3 Mod Manager",
+            style: style3.copyWith(fontSize: 40),
+          ),
         ),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          modsData.isNotEmpty
+              ? Center(
+                  child: ModList(
+                      modsData: modsData, extractedModsData: extractedModsData),
+                )
+              : Center(
+                  child: Text("No Data", style: style3),
+                ),
+          SizedBox(
+            width: 100,
+          ),
+          OptionList(),
+        ],
       ),
     );
   }
