209d9ca76cd9bff8c8b391a04cedf34ede195f53
kingdrofd <kingdrofd@gmail.com>
2024-05-20 14:31:18 +0000

    add: instruction dialog - added an instruction dialog for how to use the mod manager

--- lib/components/options/options.dart
+++ lib/components/options/options.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:general_mod_manager/models/mod_info_model.dart';
-import 'package:general_mod_manager/models/mod_paths_model.dart';
+import 'package:general_mod_manager/services/log_provider.dart';
+import 'package:general_mod_manager/services/mod_info_manager/mod_info_manager.dart';
+import 'package:general_mod_manager/services/mod_paths.dart';
 import 'package:general_mod_manager/services/directory_handler.dart';
 import 'package:general_mod_manager/services/game_configurator.dart';
 import 'package:general_mod_manager/services/script_handler.dart';
@@ -13,10 +16,14 @@ import 'package:general_mod_manager/utils/useful_functions.dart';
 import 'package:general_mod_manager/utils/variables.dart';
 import 'package:general_mod_manager/widgets/options/option_dialog.dart';
 import 'package:general_mod_manager/widgets/options/option_tile.dart';
+import 'package:provider/provider.dart';
 
 class OptionList extends StatefulWidget {
-  const OptionList({super.key});
-
+  const OptionList({
+    super.key,
+    required this.refresh,
+  });
+  final Function() refresh;
   @override
   State<OptionList> createState() => _OptionListState();
 }
@@ -26,14 +33,107 @@ class _OptionListState extends State<OptionList> {
   UrlLaunch urlLaunch = UrlLaunch();
   Directories directories = Directories();
   ModPaths modsInfo = ModPaths();
-  Scripts scripts = Scripts();
+  late ModInfoManager infoManager;
+  List order = [];
+  int increment = 1;
+  @override
+  void initState() {
+    final logProvider = Provider.of<LogProvider>(context, listen: false);
+    infoManager = ModInfoManager(directories, logProvider);
+    super.initState();
+  }
+
+  void increaseOrder() {
+    order.add(increment++);
+  }
+
+  void showConfigurationDialog() async {
+    showDialog(
+        context: context,
+        builder: (BuildContext builder) {
+          return Dialog(
+            backgroundColor: backgroundColor,
+            child: const SizedBox(
+              width: double.minPositive,
+              height: 200,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        });
+  }
+
+  void _runConfiguration() async {
+    final logProvider = Provider.of<LogProvider>(context, listen: false);
+    try {
+      GameConfigurator configurator = GameConfigurator(
+        logProvider: logProvider,
+        directories: directories,
+        gameLocalAppPath: r"Larian Studios\Baldur's Gate 3\Mods",
+        gameModSettingsPath:
+            r"Larian Studios\Baldur's Gate 3\PlayerProfiles\Public",
+        gameDataPath:
+            r"C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data",
+        gameBinPath:
+            r"C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\bin",
+        gameExePath:
+            r"C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\bin\bg3.exe",
+        scriptExtenderPath:
+            r"C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\bin", // Assuming this is the same as gameBinPath
+      );
+
+      // Configure the game
+      await configurator.configureGame();
+
+      showConfigurationDialog();
+      await Future.delayed(Duration(seconds: 2));
+      if (!mounted) return;
+      Navigator.of(context).pop();
+
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return Dialog(
+              backgroundColor: backgroundColor,
+              child: SizedBox(
+                width: 400,
+                height: 200,
+                child: Center(
+                  child: Text(
+                    "Game Configuration Successful!",
+                    style: style3.copyWith(fontSize: 20),
+                  ),
+                ),
+              ),
+            );
+          });
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: 300,
-      child: ListView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          OptionTile(
+              title: "How To Use",
+              onTap: () {
+                optionDialog.instructionDialog(context, () {
+                  _runConfiguration();
+                }, widget.refresh);
+              }),
+          Gap(8),
+          OptionTile(
+            title: "Run Configuration",
+            onTap: () {
+              _runConfiguration();
+            },
+          ),
           SizedBox(
             height: 8,
           ),
@@ -41,6 +141,10 @@ class _OptionListState extends State<OptionList> {
               title: "Get Native Loader", onTap: urlLaunch.openNativeMod),
           SizedBox(
             height: 8,
+          ),
+          OptionTile(
+            title: "Get Mod Fixer",
+            onTap: urlLaunch.openModFixer,
           ),
           OptionTile(
             title: "Open Nexu Mods",
@@ -58,38 +162,32 @@ class _OptionListState extends State<OptionList> {
           SizedBox(
             height: 8,
           ),
+
           OptionTile(
             title: "Refresh Mod List",
             onTap: () {
-              modsInfo.loadModList();
+              widget.refresh();
             },
           ),
-          OptionTile(
-            title: "Check Directory",
-            onTap: () {
-              print(checkFile(
-                  "${directories.getScriptFilesPath().path}\\installMod.exe"));
-              print(checkFile(
-                  "${directories.getScriptFilesPath().path}\\uninstallMod.exe"));
-              print(checkFile(
-                  "${directories.getScriptFilesPath().path}\\setConfiguration.exe"));
-            },
-          ),
-          OptionTile(
-            title: "Run Configuration",
-            onTap: () {
-              GameConfigurator configurator = GameConfigurator(
-                gameLocalAppPath: r"\Larian Studios\Baldur's Gate 3\Mods",
-                gameDataPath: r"",
-                gameBinPath: r"",
-                gameExePath: r"",
-                scriptExtenderPath: r"",
-              );
 
-              // Configure the game
-              configurator.configureGame();
+          OptionTile(
+            title: "Increase order",
+            onTap: () {
+              setState(() {
+                increaseOrder();
+              });
+              final logProvider =
+                  Provider.of<LogProvider>(context, listen: false);
+              logProvider.addLog(order.last.toString());
             },
           ),
+
+          // OptionTile(
+          //   title: "Export Order",
+          //   onTap: () {
+          //     infoManager.getModInfo();
+          //   },
+          // ),
         ],
       ),
     );
--- lib/widgets/options/option_dialog.dart
+++ lib/widgets/options/option_dialog.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:general_mod_manager/services/directory_handler.dart';
+import 'package:general_mod_manager/services/url_launch.dart';
 import 'package:general_mod_manager/utils/variables.dart';
+import 'package:general_mod_manager/widgets/options/option_tile.dart';
 
 class OptionDialog {
-  updateDialog(BuildContext context) {
+  Directories directories = Directories();
+  UrlLaunch urlLaunch = UrlLaunch();
+  updateDialog(BuildContext context) async {
+    _showUpdateManager(context);
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.of(context).pop();
     return showDialog(
       context: context,
       builder: (context) {
@@ -12,9 +20,7 @@ class OptionDialog {
             width: 400,
             height: 200,
             child: Center(
-              child:
-                  // Show loading animation if dialogLoading is true
-                  Text(
+              child: Text(
                 "You are all up to date! ^__^",
                 style: style2,
                 textAlign: TextAlign.center,
@@ -26,16 +32,129 @@ class OptionDialog {
     );
   }
 
-  loadingData(BuildContext context) {
-    return showDialog(
+  void _showUpdateManager(BuildContext context) async {
+    showDialog(
       context: context,
-      builder: (context) {
+      builder: (BuildContext builder) {
         return Dialog(
           backgroundColor: backgroundColor,
-          child: Container(
-            width: 400,
-            height: 400,
-            child: Center(child: CircularProgressIndicator()),
+          child: SizedBox(
+            width: double.minPositive,
+            height: 200,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void instructionDialog(
+      BuildContext context, Function onTap, Function refresh) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            "How to use the mod manager",
+            style: style3.copyWith(fontSize: 20),
+          ),
+          backgroundColor: backgroundColor,
+          content: Container(
+            width: 600,
+            height: 600,
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text(
+                    "First run the configuration button Or ",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: OptionTile(
+                    title: "Run It Now!",
+                    onTap: () {
+                      onTap();
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "This will create a folder for mods "
+                    "and other data neccessary for the app"
+                    " to work.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "You will now be able to see the mods folder that was created.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: OptionTile(
+                    title: "Open Mod Folder",
+                    onTap: () async {
+                      _showUpdateManager(context);
+                      await directories.openModsDir();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "That is the folder where you will copy your zipped mod files.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Next you can refresh the mod list by pressing the 'Refresh Mod List' button.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: OptionTile(
+                    title: "Refresh",
+                    onTap: () {
+                      refresh();
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "You can now see your mods on the right.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "In case you were wondering how the loading of mod orders work,"
+                    "it is based on the order you enable the mods.",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    "Since this is an early build, you should expect"
+                    " some issues to happen, please be sure to report it at ",
+                    style: style3,
+                  ),
+                ),
+                ListTile(
+                  title: OptionTile(
+                    title: "Bug Report",
+                    onTap: () {
+                      urlLaunch.openLink(
+                          "https://github.com/KingDrofd/baldurs_gate_3_mod_manager/issues");
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
